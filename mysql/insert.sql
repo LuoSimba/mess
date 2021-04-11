3810a34a30c99670e7ab5db46446506456817684
simba-luo <r.puppy@qq.com>
2021-04-12 03:45:38 +0800

    MySQL

---
+++ mysql/insert.sql
@@ -0,0 +1,7 @@
+
+
+create table t(aa int);
+
+-- 插入记录时，没有列名也是可以的。
+insert into t() values();
+
---
+++ mysql/service.sh
@@ -0,0 +1,7 @@
+
+
+# 将 MySQL 注册成为一个服务
+
+mysqld --install MySQL --defaults-file="C:\\Windows\\my.ini"
+
+
