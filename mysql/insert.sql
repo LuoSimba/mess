d16a6e5cd0e6064522b9ea1075761fccba27b979
simba-luo <r.puppy@qq.com>
2021-04-12 14:40:56 +0800

    MySQL

--- mysql/insert.sql
+++ mysql/insert.sql
@@ -5,3 +5,16 @@ create table t(aa int);
 -- 插入记录时，没有列名也是可以的。
 insert into t() values();
 
+
+
+
+create table t (id int not null);
+
+-- Warning: ...
+set sql_mode = '';
+insert into t() values();
+
+-- Error: ...
+set sql_mode = 'STRICT_TRANS_TABLES';
+insert into t() values();
+
---
+++ mysql/select.sql
@@ -0,0 +1,8 @@
+
+
+-- 将会选择所有行
+SELECT cat_id FROM ecs_goods_cat WHERE true;
+
+-- 将会选择空
+SELECT cat_id FROM ecs_goods_cat WHERE false;
+
