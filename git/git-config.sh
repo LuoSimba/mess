0e32697154093da619de981dbf8b3989f29bf870
simba-luo <r.puppy@qq.com>
2021-04-17 21:02:31 +0800

    git 配置

---
+++ git/git-config.sh
@@ -0,0 +1,12 @@
+
+
+# Config file location
+#
+#  --global  使用全局的配置文件（在 git 安装目录中）
+#  --system  使用系统的配置文件（在当前用户 HOME 目录）
+#  --local   使用当前仓库的配置文件
+ 
+# 配置用户信息
+git config --global user.name "jim"
+git config --global user.email "a@com"
+
