a134cbe28506c44fc68bca38de03ea78fee7e0e0
LuoSimba <r.puppy@qq.com>
2021-06-10 18:27:21 +0800

    MySQL - join

--- mysql/join.sql
+++ mysql/join.sql
@@ -13,13 +13,20 @@ JOIN === CROSS JOIN === INNER JOIN;
 -- INNER JOIN is used with an ON clause,
 -- CROSS JOIN is used otherwise.
 -- 在 MySQL 里 CROSS JOIN 与 INNER JOIN 是等价的。
+-- CROSS JOIN 不允许有连接条件。(标准)
 
 
 OUTER JOIN {
     LEFT OUTER JOIN;  -- 左外连接
     RIGHT OUTER JOIN; -- 右外连接
-    FULL OUTER JOIN;
+    FULL OUTER JOIN;  -- 全外连接
 }
 
+FULL OUTER JOIN = (LEFT OUTER JOIN) UNION (RIGHT OUTER JOIN);
 
+
+-- 显式交叉连接
+select * from table_a cross join table_b;
+-- 隐式交叉连接
+select * from table_a, table_b;
 
