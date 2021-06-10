e9909a13cc5e735fbe53a6fd1b06bfd58e4abb9e
LuoSimba <r.puppy@qq.com>
2021-06-10 17:33:00 +0800

    MySQL - join

---
+++ mysql/join.sql
@@ -0,0 +1,26 @@
+
+-- 表连接
+
+INNER JOIN ; -- 内连接
+OUTER JOIN ;
+CROSS JOIN ; -- 交叉连接、笛卡尔积
+
+-- 交叉连接是内连接的一种
+
+-- In MySQL, these are syntactic equivalents:
+JOIN;
+CROSS JOIN;
+INNER JOIN;
+-- In standard SQL, they are not equivalent:
+-- INNER JOIN is used with an ON clause,
+-- CROSS JOIN is used otherwise.
+
+
+LEFT OUTER JOIN;  -- 左外连接
+RIGHT OUTER JOIN; -- 右外连接
+
+
+
+
+
+
