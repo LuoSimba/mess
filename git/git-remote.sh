3877d82975586e776636c4f9f42674bd291d0e16
simba-luo <r.puppy@qq.com>
2021-08-04 17:10:29 +0800

    git - remote

---
+++ git/git-remote.sh
@@ -0,0 +1,16 @@
+
+
+# 显示远程仓库
+git remote -v
+
+git remote show origin
+
+# 添加一个远程仓库
+git remote add origin2 "f:/a.git"
+
+# 修改仓库名称 origin --> abc
+git remote rename origin abc
+
+# 删除一个远程仓库
+git remote rm origin
+
