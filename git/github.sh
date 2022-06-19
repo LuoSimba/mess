7bb4233cdf596abd33d437386ae1d2a9a7d8d06c
simba-luo <r.puppy@qq.com>
2022-06-20 00:58:05 +0800

    git,github usage

--- git/github.sh
+++ git/github.sh
@@ -1,7 +1,4 @@
 
-# generate new ssh key
-ssh-keygen -t rsa -C "account"
-
 # try login
 ssh -T git@github.com
 
@@ -14,13 +11,3 @@ git remote rm origin
 git remote add origin git@github.com:username/*.git
 git push --set-upstream origin master
 
-
-# 如果本地要 SSH 登录到远程服务器，需要将本地的
-
-cat ~/.ssh/id_rsa.pub
-
-# 内容写入到远程服务器的
-
-~/.ssh/authorized_keys
-
-# 文件中，这样登录就可以不用再输入密码了
